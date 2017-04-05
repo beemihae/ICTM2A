63e0cd0d216e34e50c020541eec6e3541bfa2e3a
beemihae <beemihae@w156h220.wireless.ugent.be>
2017-04-05 16:24:18 +0200

    cleaned code

--- src/getPicture.java
+++ src/getPicture.java
@@ -1,4 +1,3 @@
-
 import java.awt.FlowLayout;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
@@ -24,11 +23,6 @@ import com.sun.javafx.geom.Curve;
 import org.opencv.imgcodecs.Imgcodecs;
 import java.util.List;
 
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
 import org.opencv.core.Size;
 
 public class getPicture {
@@ -37,13 +31,11 @@ public class getPicture {
 
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		String path = "/Users/beemihae/Desktop/groundfloor1.jpg";
-		String dstPathSobel = "/Users/beemihae/Desktop/Sobel.jpg";
-		String dstPathHSV = "/Users/beemihae/Desktop/HSV.jpg";
-
-		// applyHSV(path, dstPathHSV);
-
-		Mat filtImage = applySobel(path, dstPathSobel);
-
+		String dstPathSobel = "/Users/beemihae/Desktop/filtered.jpg";
+		double width = 1.34; // width of biggest square, needed to calibrate the
+								// screen
+		double height = 1.96;
+		Mat filtImage = applyFilters(path, dstPathSobel, width, height);
 	}
 
 	public static double[] RGBtoHSV(double r, double g, double b) {
@@ -85,38 +77,39 @@ public class getPicture {
 		return new double[] { h, s, v };
 	}
 
-	public static Mat applySobel(String path, String dstPath) {
+	public static Mat applyFilters(String path, String dstPath, double width, double height) {
 		Mat image = Imgcodecs.imread(path, Imgproc.COLOR_RGB2GRAY);
-		// Mat image = Imgcodecs.imread(path, 0);
+		/// Mat image = Imgcodecs.imread(path, 0); //use for Sobel
 		// Mat imgDst = new Mat(image.size());
 		Mat imgDst = Imgcodecs.imread(path);
-		System.out.println("start Sobel");
+		System.out.println("start Gaussian Threshold");
 
 		// imgDst = erodeDilate(image, 3, 3);
-
 		// Imgproc.equalizeHist(image, image);
+
 		Imgproc.GaussianBlur(image, imgDst, new Size(23, 23), 0, 0, 0);
+
 		// imgDst = erodeDilate(image,3,3);
 
 		// Imgproc.Sobel(imgDst, imgDst, CvType.CV_8UC1, 1, 0);
-		// Imgproc.Sobel(imgDst, imgDst, CvType.CV_8UC1, 0, 1);
-		// imgDst = applyContours(imgDst, "Sobel");
+		// Imgproc.Sobel(imgDst, imgDst, CvType.CV_8UC1, 0, 1); //Apply Sobel in
+		// X&Y direction
+
 		Imgproc.cvtColor(imgDst, imgDst, Imgproc.COLOR_BGR2GRAY);
 		imgDst = adaptiveThreshold(imgDst);
-		// Imgproc.Canny(imgDst, imgDst, 10, 100);
-		Mat imgDst1 = applyContours(imgDst);
-		System.out.println("Sobel done");
+		// Imgproc.Canny(imgDst, imgDst, 10, 100); //uncomment to see only the
+		// contour lines
+		System.out.println("Start Transformation");
+
+		Mat imgDst1 = applyFlatTransformation(imgDst, width, height);
+
+		System.out.println("Transformation Done");
+		System.out.println("Gaussian Threshold Done");
+
 		Imgcodecs.imwrite(dstPath, imgDst1);
 
 		System.out.println("Written to " + dstPath);
 		return imgDst;
-		/*
-		 * BufferedImage img = matToBufferedImage(imgDst); JFrame frame = new
-		 * JFrame(); frame.getContentPane().setLayout(new FlowLayout());
-		 * frame.getContentPane().add(new JLabel((Icon) new ImageIcon(img)));
-		 * frame.pack(); frame.setVisible(true);
-		 * frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 */
 	}
 
 	public static void applyHSV(String path, String dstPath) {
@@ -137,15 +130,13 @@ public class getPicture {
 
 	}
 
-	public static Mat applyContours(Mat image) {
+	public static Mat applyFlatTransformation(Mat image, double width, double height) {
 		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-	
+
 		Imgproc.findContours(image, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
 
 		double maxArea = -1;
-		MatOfPoint temp_contour = new MatOfPoint(); // the largest is at the
-		// index 0 for starting
-		// point
+		MatOfPoint temp_contour = new MatOfPoint();
 		MatOfPoint2f approxCurve = new MatOfPoint2f();
 		int number = -1;
 
@@ -157,46 +148,43 @@ public class getPicture {
 				// check if this contour is a square
 				MatOfPoint2f new_mat = new MatOfPoint2f(temp_contour.toArray());
 				MatOfPoint2f curve = new MatOfPoint2f(contours.get(idx).toArray());
-				MatOfPoint2f curveAppr = new MatOfPoint2f();
 				double peri = Imgproc.arcLength(curve, true);
-				int contourSize = (int) temp_contour.total();
 				MatOfPoint2f approxCurve_temp = new MatOfPoint2f();
 				Imgproc.approxPolyDP(new_mat, approxCurve_temp, 0.02 * peri, true);
 				maxArea = contourarea;
 				approxCurve = approxCurve_temp;
 				number = idx;
-				//System.out.println(approxCurve_temp.total());
-				
+				// System.out.println(approxCurve_temp.total()); //count of
+				// corners
+
 			}
 		}
 		// Imgproc.drawContours(image ,contours, number ,new Scalar(100, 100,
-		// 100), 10);
+		// 100), 10); //uncomment to see the maximum contour
 
 		double[] temp_double;
 		temp_double = approxCurve.get(0, 0);
 		Point p1 = new Point(temp_double[0], temp_double[1]);
-		Imgproc.circle(image, p1, 55, new Scalar(0, 0, 255));
+		// Imgproc.circle(image, p1, 55, new Scalar(0, 0, 255)); //uncomment to
+		// see the 4 corners.
 		// Imgproc.warpAffine(sourceImage, dummy, rotImage,sourceImage.size());
 		temp_double = approxCurve.get(1, 0);
 		Point p2 = new Point(temp_double[0], temp_double[1]);
-		Imgproc.circle(image, p2, 150, new Scalar(255, 255, 255));
+		// Imgproc.circle(image, p2, 150, new Scalar(255, 255, 255));
 		temp_double = approxCurve.get(2, 0);
 		Point p3 = new Point(temp_double[0], temp_double[1]);
-		Imgproc.circle(image, p3, 200, new Scalar(255, 0, 0));
+		// Imgproc.circle(image, p3, 200, new Scalar(255, 0, 0));
 		temp_double = approxCurve.get(3, 0);
 		Point p4 = new Point(temp_double[0], temp_double[1]);
-		Imgproc.circle(image, p4, 100, new Scalar(0, 0, 255));
+		// Imgproc.circle(image, p4, 100, new Scalar(0, 0, 255));
 		ArrayList<Point> source = new ArrayList<Point>();
-		ArrayList<Point> topPoints = new ArrayList<Point>();
-		ArrayList<Point> bottomPoints = new ArrayList<Point>();
-		ArrayList<Point> sortedPoints = new ArrayList<Point>();
 
 		source.add(p1);
 		source.add(p2);
 		source.add(p3);
-		source.add(p4);
-		
-		Mat imgDst = fourPointTransformation(image, source, 10, 10);
+		source.add(p4); // (0,UL),(1, DL),(2,DR),(3,UR)
+
+		Mat imgDst = fourPointTransformation(image, source, width, height);
 
 		return imgDst;
 	}
@@ -240,28 +228,32 @@ public class getPicture {
 			Point temp = new Point(original.get(i).x, original.get(i).y);
 			destination.add(temp);
 		}
-		double width = 1.34;
-		double length = 1.96;
-		double ratio = length / width;
+
+		double ratio = lengthImg / widthImg;
 		destination.get(0).x = destination.get(1).x;
-		// destination.get(0).y = (destination.get(2).x -
-		// destination.get(1).x)*ratio + destination.get(1).y;
 		destination.get(3).y = destination.get(0).y;
 		destination.get(3).x = destination.get(2).x;
 		destination.get(2).y = destination.get(1).y;
-		Imgproc.line(image, original.get(1), original.get(0), new Scalar(100, 100, 100), 20);
+		// Imgproc.line(image, original.get(1), original.get(0), new Scalar(100,
+		// 100, 100), 20); // see lines of matching points
 		Mat source = Converters.vector_Point2f_to_Mat(original);
 		Mat dst = Converters.vector_Point2f_to_Mat(destination);
 		Mat transformation = Imgproc.getPerspectiveTransform(source, dst);
-		// RectF perspectiveCorrectedBoundingRect = new
-		// RectF(mCrop.getPerspectiveCorrectedBoundingRect());
+
 		int lengthDst = (int) image.size().width;
 		int widthDst = (int) ((int) image.size().height * ratio);
 		Mat imgDst = new Mat(widthDst, lengthDst, CvType.CV_64FC1);
 		Imgproc.warpPerspective(image, image, transformation, image.size(), Imgproc.INTER_CUBIC);
-		Imgproc.resize(image, imgDst, imgDst.size());
+		Imgproc.resize(image, imgDst, imgDst.size()); // stretch the picture
 		return imgDst;
 	};
+
+	
+public static Mat adaptiveThreshold(Mat image) {
+		Imgproc.adaptiveThreshold(image, image, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 159,
+				16);
+		return image;
+	}
 
 	public static Mat erodeDilate(Mat image, int erosionSize, int dilateSize) {
 		Mat elementErode = Imgproc.getStructuringElement(Imgproc.MORPH_RECT,
@@ -270,12 +262,6 @@ public class getPicture {
 				new Size(2 * dilateSize + 1, 2 * dilateSize + 1));
 		Imgproc.erode(image, image, elementErode);
 		Imgproc.dilate(image, image, elementDilate);
-		return image;
-	}
-
-	public static Mat adaptiveThreshold(Mat image) {
-		Imgproc.adaptiveThreshold(image, image, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 159,
-				16);
 		return image;
 	}
 
