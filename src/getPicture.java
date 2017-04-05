6bf80d8a897de2bc2128d7e33ca495c956271262
beemihae <beemihae@w156h220.wireless.ugent.be>
2017-04-05 15:58:32 +0200

    imageTransformation stretched

--- src/getPicture.java
+++ src/getPicture.java
@@ -43,7 +43,7 @@ public class getPicture {
 		// applyHSV(path, dstPathHSV);
 
 		Mat filtImage = applySobel(path, dstPathSobel);
-		
+
 	}
 
 	public static double[] RGBtoHSV(double r, double g, double b) {
@@ -103,10 +103,10 @@ public class getPicture {
 		// imgDst = applyContours(imgDst, "Sobel");
 		Imgproc.cvtColor(imgDst, imgDst, Imgproc.COLOR_BGR2GRAY);
 		imgDst = adaptiveThreshold(imgDst);
-		//Imgproc.Canny(imgDst, imgDst, 10, 100);
-		imgDst = applyContours(imgDst);
+		// Imgproc.Canny(imgDst, imgDst, 10, 100);
+		Mat imgDst1 = applyContours(imgDst);
 		System.out.println("Sobel done");
-		Imgcodecs.imwrite(dstPath, imgDst);
+		Imgcodecs.imwrite(dstPath, imgDst1);
 
 		System.out.println("Written to " + dstPath);
 		return imgDst;
@@ -139,159 +139,101 @@ public class getPicture {
 
 	public static Mat applyContours(Mat image) {
 		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-		/*
-		 * if (type.equals("Sobel")) { Imgproc.cvtColor(image, image,
-		 * Imgproc.COLOR_BGR2GRAY); }
-		 */
-		//Imgproc.Canny(image, image, 10, 100);
-		//Imgproc.GaussianBlur(image, image, new Size(23, 23), 0, 0, 0);
-		
+	
 		Imgproc.findContours(image, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
 
-
-        double maxArea = -1;
-        MatOfPoint temp_contour = new MatOfPoint(); // the largest is at the
-        // index 0 for starting
-        // point
-        MatOfPoint2f approxCurve = new MatOfPoint2f();
-        int number = -1;
-
-        for (int idx = 0; idx < contours.size(); idx++) {
-            temp_contour = contours.get(idx);
-            double contourarea = Imgproc.contourArea(temp_contour);
-            // compare this contour to the previous largest contour found
-            if (contourarea > maxArea) {
-                // check if this contour is a square
-                MatOfPoint2f new_mat = new MatOfPoint2f(temp_contour.toArray());
-                MatOfPoint2f curve = new MatOfPoint2f(contours.get(idx).toArray());
-				MatOfPoint2f curveAppr = new MatOfPoint2f();
-				double peri = Imgproc.arcLength(curve, true);
-                int contourSize = (int) temp_contour.total();
-                MatOfPoint2f approxCurve_temp = new MatOfPoint2f();
-                Imgproc.approxPolyDP(new_mat, approxCurve_temp, 0.02 * peri, true);
-                maxArea = contourarea;
-                approxCurve = approxCurve_temp;
-                number = idx;
-                System.out.println(approxCurve_temp.total());
-                /*if (approxCurve_temp.total() == 4) {
-                    maxArea = contourarea;
-                    approxCurve = approxCurve_temp;
-                    
-                }*/
-            }
-        }
-        //Imgproc.drawContours(image ,contours, number ,new Scalar(100, 100, 100), 10);
-        
-        double[] temp_double;
-        temp_double = approxCurve.get(0, 0);
-        Point p1 = new Point(temp_double[0], temp_double[1]);
-        Imgproc.circle(image,p1,55,new Scalar(0,0,255));
-        // Imgproc.warpAffine(sourceImage, dummy, rotImage,sourceImage.size());
-        temp_double = approxCurve.get(1, 0);
-        Point p2 = new Point(temp_double[0], temp_double[1]);
-        Imgproc.circle(image,p2,150,new Scalar(255,255,255));
-        temp_double = approxCurve.get(2, 0);
-        Point p3 = new Point(temp_double[0], temp_double[1]);
-        Imgproc.circle(image,p3,200,new Scalar(255,0,0));
-        temp_double = approxCurve.get(3, 0);
-        Point p4 = new Point(temp_double[0], temp_double[1]);
-        Imgproc.circle(image,p4,100,new Scalar(0,0,255));
-        ArrayList<Point> source = new ArrayList<Point>();
-        ArrayList<Point> topPoints = new ArrayList<Point>();
-        ArrayList<Point> bottomPoints = new ArrayList<Point>();
-        ArrayList<Point> sortedPoints = new ArrayList<Point>();
-
-        source.add(p1);
-        source.add(p2);
-        source.add(p3);
-        source.add(p4);
-
-
-
-      
-		/*Imgproc.findContours(image, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
 		double maxArea = -1;
-		MatOfPoint curveApprRight = new MatOfPoint();
-		for (int i = 0; i < contours.size(); i++) {
+		MatOfPoint temp_contour = new MatOfPoint(); // the largest is at the
+		// index 0 for starting
+		// point
+		MatOfPoint2f approxCurve = new MatOfPoint2f();
+		int number = -1;
 
-			double contourarea = Imgproc.contourArea(contours.get(i));
+		for (int idx = 0; idx < contours.size(); idx++) {
+			temp_contour = contours.get(idx);
+			double contourarea = Imgproc.contourArea(temp_contour);
+			// compare this contour to the previous largest contour found
 			if (contourarea > maxArea) {
-				MatOfPoint2f curve = new MatOfPoint2f(contours.get(i).toArray());
+				// check if this contour is a square
+				MatOfPoint2f new_mat = new MatOfPoint2f(temp_contour.toArray());
+				MatOfPoint2f curve = new MatOfPoint2f(contours.get(idx).toArray());
 				MatOfPoint2f curveAppr = new MatOfPoint2f();
 				double peri = Imgproc.arcLength(curve, true);
-
-				Imgproc.approxPolyDP(curve, curveAppr, 0.02 * peri, false);
-				if (curveAppr.total() == 4) { // different shapes found with 4
-												// points so add maximum area
-					curveApprRight = contours.get(i);
-					maxArea = contourarea;
-					System.out.println(contourarea + "Found");
-				}
-				;
+				int contourSize = (int) temp_contour.total();
+				MatOfPoint2f approxCurve_temp = new MatOfPoint2f();
+				Imgproc.approxPolyDP(new_mat, approxCurve_temp, 0.02 * peri, true);
+				maxArea = contourarea;
+				approxCurve = approxCurve_temp;
+				number = idx;
+				//System.out.println(approxCurve_temp.total());
+				
 			}
-			;
 		}
+		// Imgproc.drawContours(image ,contours, number ,new Scalar(100, 100,
+		// 100), 10);
+
 		double[] temp_double;
-        temp_double = curveApprRight.get(0, 0);
-        Point p1 = new Point(temp_double[0], temp_double[1]);
-        Point p2 = new Point(temp_double[0], temp_double[1]);
-        temp_double = curveApprRight.get(2, 0);
-        Point p3 = new Point(temp_double[0], temp_double[1]);
-        temp_double = curveApprRight.get(3, 0);
-        Point p4 = new Point(temp_double[0], temp_double[1]);
-        ArrayList<Point> source = new ArrayList<Point>();  //convention upper-left/upper-right/low-right/low-left
-        source.add(p1);
-        source.add(p2);
-        source.add(p3);
-        source.add(p4);
-        ArrayList<Point> sortedPoints = new ArrayList<Point>();
-        sortedPoints = sortPoints(source);
-        */
+		temp_double = approxCurve.get(0, 0);
+		Point p1 = new Point(temp_double[0], temp_double[1]);
+		Imgproc.circle(image, p1, 55, new Scalar(0, 0, 255));
+		// Imgproc.warpAffine(sourceImage, dummy, rotImage,sourceImage.size());
+		temp_double = approxCurve.get(1, 0);
+		Point p2 = new Point(temp_double[0], temp_double[1]);
+		Imgproc.circle(image, p2, 150, new Scalar(255, 255, 255));
+		temp_double = approxCurve.get(2, 0);
+		Point p3 = new Point(temp_double[0], temp_double[1]);
+		Imgproc.circle(image, p3, 200, new Scalar(255, 0, 0));
+		temp_double = approxCurve.get(3, 0);
+		Point p4 = new Point(temp_double[0], temp_double[1]);
+		Imgproc.circle(image, p4, 100, new Scalar(0, 0, 255));
+		ArrayList<Point> source = new ArrayList<Point>();
+		ArrayList<Point> topPoints = new ArrayList<Point>();
+		ArrayList<Point> bottomPoints = new ArrayList<Point>();
+		ArrayList<Point> sortedPoints = new ArrayList<Point>();
 
-		//Rect rect = Imgproc.boundingRect(curveApprRight);
-		image = fourPointTransformation(image, source, 10,10);
-		//System.out.println(rect.height);
-
-	//	System.out.println(rect.x + "," + rect.y + "," + rect.height + "," + rect.width);
-
-		//Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),new Scalar(100, 100, 100),10);
+		source.add(p1);
+		source.add(p2);
+		source.add(p3);
+		source.add(p4);
 		
-		return image;
+		Mat imgDst = fourPointTransformation(image, source, 10, 10);
+
+		return imgDst;
 	}
 
-	public static ArrayList<Point> sortPoints(ArrayList<Point> source){
+	public static ArrayList<Point> sortPoints(ArrayList<Point> source) {
 		double averageY = 0;
 		ArrayList<Point> upperPoints = new ArrayList<Point>();
 		ArrayList<Point> lowerPoints = new ArrayList<Point>();
 		ArrayList<Point> Points = new ArrayList<Point>();
 		for (int i = 0; i < source.size(); i++) {
-			averageY+= source.get(i).y;
+			averageY += source.get(i).y;
 		}
-		averageY = averageY/source.size();
+		averageY = averageY / source.size();
 		for (int i = 0; i < source.size(); i++) {
-			if(source.get(i).y < averageY){
+			if (source.get(i).y < averageY) {
 				lowerPoints.add(source.get(i));
-			}else upperPoints.add(source.get(i));
+			} else
+				upperPoints.add(source.get(i));
 		}
-		if(upperPoints.get(0).x < upperPoints.get(1).x){
+		if (upperPoints.get(0).x < upperPoints.get(1).x) {
 			Points.add(upperPoints.get(0));
 			Points.add(upperPoints.get(1));
-		}else{
+		} else {
 			Points.add(upperPoints.get(1));
 			Points.add(upperPoints.get(0));
 		}
-		if(lowerPoints.get(0).x > lowerPoints.get(1).x){
+		if (lowerPoints.get(0).x > lowerPoints.get(1).x) {
 			Points.add(lowerPoints.get(0));
 			Points.add(lowerPoints.get(1));
-		}else{
+		} else {
 			Points.add(lowerPoints.get(1));
 			Points.add(lowerPoints.get(0));
 		}
 		return Points;
-		
+
 	}
-	
-	
+
 	public static Mat fourPointTransformation(Mat image, ArrayList<Point> original, double widthImg, double lengthImg) {
 		ArrayList<Point> destination = new ArrayList<Point>();
 		for (int i = 0; i < original.size(); i++) {
@@ -302,15 +244,23 @@ public class getPicture {
 		double length = 1.96;
 		double ratio = length / width;
 		destination.get(0).x = destination.get(1).x;
-		destination.get(0).y = destination.get(3).y;
+		// destination.get(0).y = (destination.get(2).x -
+		// destination.get(1).x)*ratio + destination.get(1).y;
+		destination.get(3).y = destination.get(0).y;
 		destination.get(3).x = destination.get(2).x;
 		destination.get(2).y = destination.get(1).y;
-		Imgproc.line(image, original.get(1),original.get(0), new Scalar(100, 100, 100), 20);
+		Imgproc.line(image, original.get(1), original.get(0), new Scalar(100, 100, 100), 20);
 		Mat source = Converters.vector_Point2f_to_Mat(original);
 		Mat dst = Converters.vector_Point2f_to_Mat(destination);
 		Mat transformation = Imgproc.getPerspectiveTransform(source, dst);
-		Imgproc.warpPerspective(image, image, transformation, image.size(),Imgproc.INTER_CUBIC);
-		return image;
+		// RectF perspectiveCorrectedBoundingRect = new
+		// RectF(mCrop.getPerspectiveCorrectedBoundingRect());
+		int lengthDst = (int) image.size().width;
+		int widthDst = (int) ((int) image.size().height * ratio);
+		Mat imgDst = new Mat(widthDst, lengthDst, CvType.CV_64FC1);
+		Imgproc.warpPerspective(image, image, transformation, image.size(), Imgproc.INTER_CUBIC);
+		Imgproc.resize(image, imgDst, imgDst.size());
+		return imgDst;
 	};
 
 	public static Mat erodeDilate(Mat image, int erosionSize, int dilateSize) {
