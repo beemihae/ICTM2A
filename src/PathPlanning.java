3b50b84c6b5be774a440fc2b7401724d4d5de6eb
codecracker <olivier.degroote@live.be>
2017-04-16 19:42:33 +0200

    Update PathPlanning.java

--- src/PathPlanning.java
+++ src/PathPlanning.java
@@ -56,11 +56,11 @@ public class PathPlanning {
    \\ ...               ...               ...               ...
    
    public static void main(String[] args){
- public AstarSearchAlgorithm alg = new AstarSearchAlgorithm();
- public int dimx = 1030;
- public int dimy = 2048;
- public float heading = 45;
- public Point  arr[][] = new Point[][];
+ public AstarSearchAlgorithm alg = new AstarSearchAlgorithm(); \\ nieuw algoritme maken om later te gebruiken
+ public int dimx = 1030;					\\ x dimensie van de foto
+ public int dimy = 2048;					\\ y dimensie van de foto
+ public float heading = 45;					\\ hoek met de x as van de startrichting van de robot
+ public Point  arr[][] = new Point[][];				\\ input die normaal van elias komt
 arr[0][0]=new Point(10 , 10);  
 arr[1][0]=new Point(5 , 95);  
 arr[1][1]=new Point(95 , 95);  
@@ -76,11 +76,11 @@ arr[3][2]=new Point(65 , 75);
 arr[3][3]=new Point(70 , 75);
 arr[3][4]=new Point(70 , 60);
 arr[3][5]=new Point(25 , 60);
- public Point end = new Point(85,85);
-public WayPoint endpoint = new WayPoint(end);
-public Pose startpoint = new Pose(arr[0][0].getX,arr[0][0].getY,heading);
-  public Rectangle boundingRect = new Rectangle(0, dimy, dimx, dimy);
- public line lines[] = new line[];
+ public Point end = new Point(85,85);				\\ zelfgekozen eindpunt
+public WayPoint endpoint = new WayPoint(end);			\\ formaat van eindpunt voor in de methode van pathplanning
+public Pose startpoint = new Pose(arr[0][0].getX,arr[0][0].getY,heading);	\\ startpunt uit elias zijn input
+  public Rectangle boundingRect = new Rectangle(0, dimy, dimx, dimy);		\\ rechthoek rond foto door dimensies geconstrueerd
+ public line lines[] = new line[];						\\ lijnen construeren voor in de rechthoek
       for(int i=0;i<dimx;i++){
       lines[i] = Line(i+1, 0, i+1, dimy);
       }
@@ -88,14 +88,14 @@ public Pose startpoint = new Pose(arr[0][0].getX,arr[0][0].getY,heading);
       lines[j+dimx] = Line(0, j+1, dimx , j+1);
       }
       
- public LineMap map = new LineMap( lines, boundingRect);
+ public LineMap map = new LineMap( lines, boundingRect);			\\ een map maken van de lijnenrij hierboven
 	   map.createSVGFile(linemap);
- public GridMesh mesh = new GridMesh(map, 1.2060546875 , 0.25);
- public NodePathFinder Pathfinder = new NodePathFinder(alg, mesh); 
- public Path shortestpath = Pathfinder.findRoute(startpoint,endpoint);
+ public GridMesh mesh = new GridMesh(map, 1.2060546875 , 0.25, arr);			\\ mesh maken van de map voor in de methode
+ public NodePathFinder Pathfinder = new NodePathFinder(alg, mesh); 		\\ nieuwe nodepath voor in de methode
+ public Path shortestpath = Pathfinder.findRoute(startpoint,endpoint);		\\ het momenteel kortste pad
  system.out.println(shortestpath.tostring());
    }
-class GridMesh extends FourWayGridMesh{
+class GridMesh extends FourWayGridMesh{						\\ nieuwe constructor die punten van obstakels weglaat
 public GridMesh(LineMap map, float gridSpace, float clearance, Point[][] arr)  {
 		super(map,gridspace,clearance);
 	
@@ -201,6 +201,9 @@ public GridMesh(LineMap map, float gridSpace, float clearance, Point[][] arr)  {
 						system.out.println(right);
 					}
 				}	
+		}
+		break;
+		default{
 		}
 		break;
 	    	}
