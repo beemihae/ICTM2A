971dd358783bf919fb84dcae87fcefedc42b6f99
beemihae <beemihae@MacBook-Pro-2.local>
2017-04-28 23:06:39 +0200

    niet werkende pilot, wel inlezen

---
+++ src/testImageProcessor.java
@@ -0,0 +1,13 @@
+
+public class testImageProcessor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ImageProcessor2 test = new ImageProcessor2();
+		test.getContours();
+		
+	}
+	
+	
+
+}
